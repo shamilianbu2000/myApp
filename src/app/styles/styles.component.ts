b48fe2d1c009181122ab7bdf75011b333102f625
shamilianbu2000 <shamilianbu2000@gmail.com>
2022-09-19 15:31:46 +0530

    for and if condition do

--- src/app/styles/styles.component.ts
+++ src/app/styles/styles.component.ts
@@ -7,21 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StylesComponent implements OnInit {
   
+
   constructor() { }
-  
+
+  isActive : boolean = true;
+  names = ['meena','aki','surya']
+  list:object = [{'id':1,name:"developer"},{id:2,name:"programer"}]
+  ngOnInit(): void {
+  }
+
+
+
+}
 
  
   
 
 
-  ngOnInit(): void {
-  }
-
+ 
 
   
 
   
-}
 
 
 
